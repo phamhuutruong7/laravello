4f360391241627905f1ab5187aceff44b79d68f2
Pham Huu Truong <t.pham@sup-scs.de>
2021-04-01 14:36:06 +0200

    Setup Vue Apollo & JS Import Syntax
    Set Vue Apollo and js import syntax

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,13 @@
-require('./bootstrap');
+import './bootstrap';
+import Vue from 'vue';
+import Board from './Board';
+import apolloProvider from './apollo.config';
 
-window.Vue = require('vue');
+window.Vue = Vue;
 
 Vue.component('Board', require('./Board.vue').default);
 
 const app = new Vue({
     el: '#app',
+    apolloProvider
 });
