03a44f3429192e93d75d9aa6acb44d05fe63dae0
Pham Huu Truong <t.pham@sup-scs.de>
2021-04-06 13:24:40 +0200

    Update Query Cache after Mutation
    Make the CardList update after click Add new card.

--- resources/js/components/CardAddButton.vue
+++ resources/js/components/CardAddButton.vue
@@ -6,6 +6,7 @@
 
 <script>
 import CardAdd from "./../graphql/CardAdd.gql";
+import BoardQuery from "./../graphql/BoardWithListsAndCards.gql";
 export default {
     methods: {
         addCard(){
@@ -15,6 +16,14 @@ export default {
                     title: 'Added through mutation',
                     listId: 1,
                     order: 1
+                },
+                update(store, { data: { cardAdd }}){
+                    const data = store.readQuery({
+                        query: BoardQuery,
+                        variables: { id: 1}
+                    });
+                    data.board.lists.find(list => (list.id = 1)).cards.push(cardAdd);
+                    store.writeQuery({ query: BoardQuery, data});
                 }
             });
         }
