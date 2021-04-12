1780b36b0aa4c52a3c181119986fbca4c4091bcb
Pham Huu Truong <t.pham@sup-scs.de>
2021-04-12 13:01:08 +0200

    Group Hover & Transitions
    Group Hover & Transitions

--- public/js/app.js
+++ public/js/app.js
@@ -5684,6 +5684,13 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
   props: {
     card: Object
@@ -31671,12 +31678,34 @@ var render = function() {
     "div",
     {
       staticClass:
-        "bg-white shadow-card card rounded-sm p-2 cursor-pointer text-sm hover:bg-gray-100 mb-2"
+        "group bg-white shadow-card rounded-sm p-2 cursor-pointer text-sm hover:bg-gray-100 mb-2 flex justify-between"
     },
-    [_vm._v("\n    " + _vm._s(_vm.card.title) + "\n")]
+    [_c("div", [_vm._v(_vm._s(_vm.card.title))]), _vm._v(" "), _vm._m(0)]
   )
 }
-var staticRenderFns = []
+var staticRenderFns = [
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c(
+      "div",
+      {
+        staticClass:
+          "flex font-bold opacity-0 group-hover:opacity-100 transition-opacity ease-out duration-500"
+      },
+      [
+        _c("div", { staticClass: "text-gray-400 pr-2 hover:text-yellow-700" }, [
+          _vm._v("E")
+        ]),
+        _vm._v(" "),
+        _c("div", { staticClass: "text-gray-400 hover:text-red-700" }, [
+          _vm._v("D")
+        ])
+      ]
+    )
+  }
+]
 render._withStripped = true
 
 
