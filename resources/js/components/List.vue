bee884ee75ac388599e190072c97bc6a7e045c3c
Pham Huu Truong <t.pham@sup-scs.de>
2021-04-12 14:22:27 +0200

    Delete Card, and updating query cache.
    Follow the click event to know the structure

--- resources/js/components/List.vue
+++ resources/js/components/List.vue
@@ -1,34 +1,41 @@
 <template>
     <div class="bg-gray-300 rounded-sm p-2 mr-2 list">
         <div class="flex justify-between">
-            <div class="text-gray-800 pl-2 ob-2 font-bold">{{ list.title }}</div>
+            <div class="text-gray-800 pl-2 pb-2 font-bold">{{ list.title }}</div>
         </div>
-        <Card v-for="card in list.cards" :key="card.id" :card="card"></Card>
-        <CardEditor v-if="editing" @closed="editing=false" :list="list"></CardEditor>
-        <CardAddButton v-else @click="editing = true"></CardAddButton>
 
+        <Card
+            v-for="card in list.cards"
+            :key="card.id"
+            :card="card"
+            @deleted="$emit('card-deleted', {...$event, listId: list.id})"
+        ></Card>
+
+        <CardEditor
+            v-if="editing"
+            @closed="editing=false"
+            :list="list"
+            @added="$emit('card-added', {...$event, listId: list.id})"
+        ></CardEditor>
+        <CardAddButton v-else @click="editing=true"></CardAddButton>
     </div>
 </template>
 
 <script>
-import Card from './Card';
-import CardAddButton from './CardAddButton';
+import Card from "./Card";
+import CardAddButton from "./CardAddButton";
 import CardEditor from "./CardEditor";
-export default{
-    components: {
-        Card,
-        CardAddButton,
-        CardEditor
-    },
+export default {
+    components: { Card, CardAddButton, CardEditor },
     props: {
         list: Object
     },
-    data(){
+    data() {
         return {
             editing: false
-        }
+        };
     }
-}
+};
 </script>
 
 <style scoped>
