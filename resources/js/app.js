89c986f2bb62dde08db665e9692f7ae374eae37f
Pham Huu Truong <t.pham@sup-scs.de>
2021-04-26 10:18:17 +0200

    Vuex Installation and Configuration

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@ import Vue from 'vue';
 import Board from './Board';
 import apolloProvider from './components/apollo.config';
 import router from "./router.config";
+import store from "./vuex.config";
 
 window.Vue = Vue;
 
@@ -11,5 +12,6 @@ Vue.component('Board', require('./Board.vue').default);
 const app = new Vue({
     el: '#app',
     apolloProvider,
-    router
+    router,
+    store,
 });
